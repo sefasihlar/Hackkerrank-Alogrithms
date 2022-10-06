de8eb292a5f9ff96287c0f642e4c9464199e50cb
ayyıldız@software <sihlarsefa7@gmail.com>
2022-10-06 13:35:04 +0300

    Github push Algorithms

---
+++ BirthdayCakeCandles.py
@@ -0,0 +1,31 @@
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+
+def birthdayCakeCandles(candles):
+    n = len(candles)
+    maxnum = 0
+    count = 0
+    for i in range(n):
+        if candles[i]>maxnum:
+            maxnum = candles[i]
+            count = 1
+        elif candles[i] == maxnum:
+            count+=1
+    return count
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ deneme.py
@@ -0,0 +1,4 @@
+x = input("bir sayi gir")
+def myround(x, base=5):
+    return base * round(x/base)
+myround()
